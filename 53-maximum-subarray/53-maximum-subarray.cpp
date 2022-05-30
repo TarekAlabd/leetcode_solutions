843d16d1b0d2ccaf5c18d2896a5cf8f265615ef7
Tarek Alabd <tarekalabd1@gmail.com>
2022-05-30 19:35:16 +0200

    Time: 248 ms (5.07%), Space: 67.8 MB (11.75%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -3,11 +3,9 @@ public:
     int maxSubArray(vector<int>& nums) {
         int max_sum = INT_MIN, current_sum=0;
         for (auto i:nums) {
-            if (current_sum < 0) {
-                current_sum=0;
-            }
             current_sum+=i;
             max_sum = max(max_sum, current_sum);
+            current_sum = max(0, current_sum);
         }
         return max_sum;
     }
