a4825481fb5e1896bd9bc4be9dd5e328f773146e
Tarek Alabd <tarekalabd1@gmail.com>
2022-06-06 19:58:21 +0200

    Time: 7 ms (24.20%), Space: 9.2 MB (30.82%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,23 +4,17 @@ public:
         int i = m-1, j=n-1, index=(m+n)-1;
         while (i>=0 && j>=0) {
             if (nums1[i]>=nums2[j]) {
-                nums1[index] = nums1[i];
-                i--;
+                nums1[index] = nums1[i--];
             } else {
-                nums1[index] = nums2[j];
-                j--;
+                nums1[index] = nums2[j--];
             }
             index--;
         }
         while (i>=0) {
-            nums1[index] = nums1[i];
-            i--;
-            index--;
+            nums1[index--] = nums1[i--];
         }
         while (j>=0) {
-            nums1[index] = nums2[j];
-            j--;
-            index--;
+            nums1[index--] = nums2[j--];
         }
     }
 };
