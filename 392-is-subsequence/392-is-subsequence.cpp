78602e85341415b6d0caad22343c1b3bd9900710
Tarek Alabd <tarekalabd1@gmail.com>
2022-07-20 01:12:12 +0200

    Time: 5 ms (28.30%), Space: 6.4 MB (28.52%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=s.size()-1, j=t.size()-1;
+        while (i>=0 && j>=0) {
+            if (s[i] == t[j]) {
+                i--,j--;
+            } else {
+                j--;
+            }
+        }
+        if (i>=0) {
+            return false;
+        }
+        return true;
+    }
+};
